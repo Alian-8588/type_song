8d5768fe393171d658353ffcf360eac6ae929636
Alian <la_8588@outlook.com>
2024-09-25 14:13:30 +0100

    yes

---
+++ type_song.py
@@ -0,0 +1,11 @@
+# problem 1
+song_line = input ("type the first line of your favourite song")
+
+length_line = len(song_line)
+
+print(f"the length of first line is {length_line}")
+
+start = int(input("the starting index"))
+end = int(input("the ending index"))
+
+print(f" from {start} to end {end} is: {song_line[start:end]}")
